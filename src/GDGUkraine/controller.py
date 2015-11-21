c4850554dc214088610ac16ec4c70855486b2211
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-11-21 16:06:12 +0200

    PEP8 refactoring of the main controller module

--- src/GDGUkraine/controller.py
+++ src/GDGUkraine/controller.py
@@ -1,19 +1,17 @@
 import cherrypy
-import social
-import json
 import logging
+import functools
 
 from blueberrypy.template_engine import get_template
-
-from cherrypy import HTTPError, HTTPRedirect
 
 from .auth_controller import AuthController
 
 
 logger = logging.getLogger(__name__)
 
+
 # decorator. apply to certain methods after 'exposed'
-def render(template = None, page_id = None, ):
+def render(template=None, page_id=None, menu=None):
     def dec(func):
         @functools.wraps(func)
         def wrapper(obj):
@@ -21,7 +19,12 @@ def render(template = None, page_id = None, ):
             obj is an object with context of func
             '''
             tmpl = get_template(template)
-            return tmpl.render(webpage = { 'content': func(obj), 'menu': obj.menu if hasattr(obj, 'menu') else menu, 'current_page': page_id })
+            return tmpl.render(webpage={
+                               'content': func(obj),
+                               'menu': (obj.menu
+                                        if hasattr(obj, 'menu')
+                                        else menu),
+                               'current_page': page_id})
         return wrapper
     return dec
 
@@ -29,90 +32,86 @@ def render(template = None, page_id = None, ):
 class Root:
 
     @cherrypy.expose
-    #@render(template = 'gdg.org.ua_old.html', page_id = 'about')
+    # @render(template = 'gdg.org.ua_old.html', page_id = 'about')
     def index(self, **kwargs):
         from .api import get_all_gdg_places
-        #tmpl = get_template("index.html")
         tmpl = get_template("gdg.org.ua_old.html")
-        return tmpl.render(places=get_all_gdg_places(cherrypy.request.orm_session, filtered = True))
+        return tmpl.render(
+            places=get_all_gdg_places(cherrypy.request.orm_session,
+                                      filtered=True))
 
     @cherrypy.expose
     def admin(self, **kwargs):
         tmpl = get_template("admin/admin.html")
-        return tmpl.render(p = {})
+        return tmpl.render(p={})
 
 
 Root.auth = AuthController()
 
-    #@cherrypy.expose
-    #def default(self, *unparsed):
-    #    print('lol')
-    #    return self.index()
 
-
-#class API:
+# class API:
 #    """It is an API router"""
-#
+
 #    @cherrypy.expose
 #    def index(self, **kwargs):
 #        return "API is privatei"
-#
+
 #    @cherrypy.expose
 #    def default(self, *unparsed):
 #        return self.index()
-#
-#class Participants:
+
+# class Participants:
 #    """docstring for Participants"""
-#
+
 #    _KEY = 'fb6a10f172177dca7fb4de9d59c46a1e'
-#
+
 #    @cherrypy.expose
 #    def index(self, **kwargs):
 #        return "API is privates"
-#
+
 #    @cherrypy.expose
 #    def default(self, **unparsed):
 #        return self.index()
-#
+
 #    @cherrypy.expose
 #    def add(self, *unparsed):
 #        return "adding" # self.index()
-#
+
 #    @cherrypy.expose
 #    def remove(self, *unparsed):
 #        return 'removing' # self.index()
-#
+
 #    @cherrypy.expose
 #    def edit(self, *unparsed):
 #        return 'changing' # self.index()
-#
-    #@cherrypy.expose
-    #def get(self, *unparsed):
-    #    return 'changing' # self.index()
 
-    #@cherrypy.expose
-    #class get:
-    #    """docstring for get"""
+#     @cherrypy.expose
+#     def get(self, *unparsed):
+#        return 'changing' # self.index()
 
-    #    #def __init__(self, **arg):
-    #        #super(ClassName, self).__init__()
-    #        #self.arg = arg
-    #    #    pass
-    #
-    #    @cherrypy.expose
-    #    def index(self, **uid):
-    #        return 'get index' # self.index()
+#     @cherrypy.expose
+#     class get:
+#        """docstring for get"""
 
-    #    @cherrypy.expose
-    #    def x(self, **uid):
-    #        return 'get index' # self.index()
+#        #def __init__(self, **arg):
+#            #super(ClassName, self).__init__()
+#            #self.arg = arg
+#        #    pass
 
-    #    @cherrypy.expose
-    #    def default(self, **unparsed):
-    #        return 'changing' # self.index()
+#        @cherrypy.expose
+#        def index(self, **uid):
+#            return 'get index' # self.index()
 
-    #get = get()
-    #get.default = default
+#        @cherrypy.expose
+#        def x(self, **uid):
+#            return 'get index' # self.index()
 
-#Root.api = API()
-#Root.api.participants = Participants()
+#        @cherrypy.expose
+#        def default(self, **unparsed):
+#            return 'changing' # self.index()
+
+#     get = get()
+#     get.default = default
+
+# Root.api = API()
+# Root.api.participants = Participants()

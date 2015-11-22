ff36bbcd7ea88862a9e558888de2c635dfb1512d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-11-22 11:16:18 +0200

    Rename check to is_admin

--- src/GDGUkraine/utils.py
+++ src/GDGUkraine/utils.py
@@ -1,7 +1,7 @@
 import cherrypy as cp
 
 
-def is_authorized():
+def is_admin():
     return isinstance(cp.session.get('admin_user'), dict) and \
         isinstance(cp.session.get('google_oauth'), dict) and \
         isinstance(cp.session.get('google_user'), dict)

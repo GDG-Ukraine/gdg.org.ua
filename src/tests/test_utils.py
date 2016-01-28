16b3247a897ca60dfc01306a0a0e2c3af5d5787d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-01-28 23:44:49 +0200

    Implement test for TableExporter

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -1,7 +1,21 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
 import cherrypy
 from blueberrypy.testing import ControllerTestCase
 
+from openpyxl import load_workbook
+
+from GDGUkraine import api, model
 from GDGUkraine.lib.utils.url import base_url, url_for
+from GDGUkraine.lib.utils.table_exporter import (
+    gen_participants_xlsx,
+    event_parsers
+)
+
+from tests.helper import DBTestFixture, orm_session, Session
 
 
 class UtilTest(ControllerTestCase):
@@ -65,3 +79,60 @@ class UtilTest(ControllerTestCase):
         for test_url in self.urls_exc_testset:
             with self.assertRaises(test_url['res']):
                 url_for(**test_url['inp'])
+
+
+class TableExporterTest(DBTestFixture, unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        # Detecting the number of Nickname column
+        for num, _ in enumerate(event_parsers):
+            if _[0] == 'Nickname':
+                self.nickname_col_num = num
+                break
+
+        session = Session()
+
+        user = model.User(
+            name='Serhii',
+            surname='Brin',
+            nickname='googler_zero',
+            email='serhii@google.com',
+            gender='male',
+        )
+        session.add(user)
+
+        host = model.Place()
+        session.add(host)
+        session.commit()
+
+        event = model.Event(
+            url='https://plus.google.com/events/xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+            title='Meeting Brin',
+            desc='Come and feel yourself Serhii\'s friend!',
+            host_gdg_id=host.id,
+        )
+        session.add(event)
+        session.commit()
+
+        self.event_registration = model.EventParticipant(
+            googler_id=user.id,
+            event_id=user.id,
+        )
+        session.add(self.event_registration)
+        session.commit()
+
+        event = api.find_event_by_id(session, event.id)
+        self.event_participations = api.find_participants_by_event(session,
+                                                                   event)
+
+    def tearDown(self):
+        super().tearDown()
+
+    @orm_session  # closes session on function finish
+    def test_gen_xlsx(self):
+        xls_file = gen_participants_xlsx(self.event_participations)
+        wb = load_workbook(xls_file)
+        ws = wb.active
+        self.assertEqual(ws.rows[1][self.nickname_col_num].value,
+                         self.event_registration.user.nickname)

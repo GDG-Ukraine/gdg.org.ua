335e47d0c521bcd76906af9e65972e2d7ccb5321
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-02-01 17:07:10 +0200

    Integrate bower while trying to add auth button to admin error page

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,4 +1,4 @@
-angular.module('gdgorgua', ['ngResource','$strap'])
+angular.module('gdgorgua', ['directive.g+signin', 'ngRoute', 'ngResource','$strap'])
     .config(function($routeProvider,$httpProvider) {
         $routeProvider.
             when('/events/', {controller:'EventsListCtrl', templateUrl:'/templates/admin/events/list.html'}).
@@ -30,10 +30,26 @@ angular.module('gdgorgua', ['ngResource','$strap'])
             }
          });
     })
-.controller('MainCtrl', function($scope, $location,$http) {
-   $scope.info = $http.get('/api/info').then(function(r) { return r.data;});
-   $scope.$watch(function() { return $location.path()}, function(nv) {
-       var parts = nv.split('/');
-       $scope.current = parts[1];
-   });
+.controller('MainCtrl', function($scope, $location, $http) {
+    $scope.info = $http.get('/api/info').then(function(r) { return r.data;});
+    $scope.$watch(function() { return $location.path()}, function(nv) {
+        var parts = nv.split('/');
+        $scope.current = parts[1];
+    });
+    $scope.$on('event:google-plus-signin-success', function (event, authResult) {
+        // User successfully authorized the G+ App!
+        console.log('Signed in!');
+        console.log(authResult);
+        // debugger
+        $http.post('/api/sign-in', {
+            id_token: authResult.hg.id_token,
+            access_code: authResult.hg.login_hint
+        }).then(function(result) {
+            console.log(result);
+        });
+    });
+    $scope.$on('event:google-plus-signin-failure', function (event, authResult) {
+        // User has not authorized the G+ App!
+        console.log('Not signed into Google Plus.');
+    });
 });

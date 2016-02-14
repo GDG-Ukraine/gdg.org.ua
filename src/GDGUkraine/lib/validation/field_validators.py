694d7b86502f8d09142b072df723a98d9c544ec2
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-02-14 20:21:28 +0200

    [#29|feature] adjust email validator to make tests pass

--- src/GDGUkraine/lib/validation/field_validators.py
+++ src/GDGUkraine/lib/validation/field_validators.py
@@ -2,9 +2,15 @@ import re
 
 
 def email(field, value, error):
-    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    if not re.match(regex, value):
+    regex = r'^[a-zA-Z0-9_.+-]+@(?P<hostname>[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)$'
+    match = re.match(regex, value)
+    if not match:
         error(field, 'Not a valid email address')
+    else:
+        hostname = match.group('hostname')
+        for section in hostname.split('.'):
+            if not section:
+                error(field, 'Not a valid email address')
 
 
 def url(field, value, error):

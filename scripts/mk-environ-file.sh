083ca85850c359e4936ce76f1a389dd0c6066c65
Oleksandr Kovalchuk <anxolerd@outlook.com>
2016-09-13 21:51:19 +0300

    Add crucial variables to environ file
    - Add APP_ROOT and others to BLUEBERRYPY_CONFIG in creation script
    - Remove APP_ROOT and APP_PACKAGE from vagga.yaml

--- scripts/mk-environ-file.sh
+++ scripts/mk-environ-file.sh
@@ -29,15 +29,21 @@ _main() {
     local config_file='environ'
 
     # Get variables
+    local app_root=`_get_user_input 'Enter full path to your app root dir (use /work to run in vagga)' "$(pwd)"`
     local hostname=`_get_user_input 'Enter hostname (start with http:// or https://)' 'http://localhost:8080'`
     local db_url=`_get_user_input 'Enter database connection dsn string' 'mysql+mysqlconnector://mysql:mysql@localhost:3307/gdg'`
     local google_client_id=`_get_user_input 'Your Google client id' '<REPLACE_THIS>.apps.googleusercontent.com'`
     local google_client_secret=`_get_user_input 'Your Google client secret' '<REPLACE_THIS>'`
 
-    # Combine all into config
-    local config='{ "global": { "base_app_url": "'${hostname}'", "key":"sadsadsadasdsadasadsadsadasdsada", "google_oauth": { "id": "'${google_client_id}'", "secret": "'${google_client_secret}'" }, "alembic": {"sqlalchemy.url": "'${db_url}'"} }, "sqlalchemy_engine": { "url": "'${db_url}'" } }'
+    local cache_dir="$app_root/.cache"
+    local templates_dir="$app_root/src/GDGUkraine/templates"
 
-    echo "BASE_URL=$hostname" > "$config_file"
+    # Combine all into config
+    local config='{ "global": { "CWD": "'${app_root}'", "base_app_url": "'${hostname}'", "key":"sadsadsadasdsadasadsadsadasdsada", "google_oauth": { "id": "'${google_client_id}'", "secret": "'${google_client_secret}'" }, "alembic": {"sqlalchemy.url": "'${db_url}'"} }, "sqlalchemy_engine": { "url": "'${db_url}'" }, "jinja2": { "bytecode_cache": { "!!python/object:jinja2.bccache.FileSystemBytecodeCache": {"directory": "'${cache_dir}'", "pattern": "__jinja2_%s.cache" } }, "loader": {"!!python/object:jinja2.loaders.FileSystemLoader": {"encoding": "utf-8", "searchpath": ["'${templates_dir}'"] } } } }'
+
+    echo "APP_ROOT=$app_root" > "$config_file"
+    echo "APP_PACKAGE=GDGUkraine" >> "$config_file"
+    echo "BASE_URL=$hostname" >> "$config_file"
     # use insecure transport if basename is not https://
     case $hostname in
         https://* ) echo "Not setting OAUTHLIB_INSECURE_TRANSPORT as HTTPS is used" ;;

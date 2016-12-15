52895dbbef7c479573c5d678d920a9757be1fa35
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2016-12-15 22:11:00 +0200

    Add dummy test for /

--- src/tests/test_controller.py
+++ src/tests/test_controller.py
@@ -1,8 +1,16 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
 from GDGUkraine.lib.testing import TestCase
 
 
 class RootTest(TestCase):
 
+    # TODO: complete this test
+    @unittest.mock.patch('GDGUkraine.api.get_all_gdg_places',
+                         new=lambda sess, filtered: [])
     def test_index(self):
         self.getPage('/')
         self.assertStatus(200)

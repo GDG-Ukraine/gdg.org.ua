ffefb6717d5752e1fde364aaf76816dd29c475d2
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-01-01 20:46:23 +0200

    Cut off useless dummy test case

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -2,7 +2,6 @@ try:
     import unittest2 as unittest
 except ImportError:
     import unittest
-from datetime import date
 
 from GDGUkraine.model import User
 
@@ -16,30 +15,11 @@ class UserTest(DBTestFixture, unittest.TestCase):
 
         session = Session()
 
-        user = User(displayname='alice in wonderland',
+        user = User(nickname='alice',
+                    name='Alice',
+                    surname='Johns',
                     email='alice@wonderland.com',
-                    password='wonderlandpass',
-                    sex='m',
-                    date_of_birth=date.today())
+                    gender='female')
 
         session.add(user)
         session.commit()
-
-    @orm_session
-    def test_passwords(self):
-
-        session = Session()
-
-        user = User(displayname='displayname',
-                    email='abc@example.com',
-                    sex='m',
-                    date_of_birth=date(1980, 1, 1))
-        user.password = 'password'
-        session.add(user)
-        session.commit()
-
-        self.assertTrue(user.validate_password('password'))
-        from base64 import b64decode
-        self.assertEqual(
-            user._User__encrypt_password('password', b64decode(user.salt)),
-            user.password)

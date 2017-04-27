760561bba1a8ec0255bbeef4d5bb20f07db11b8d
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-04-27 20:21:19 +0300

    Implement deps management via pbr. Closes #110

--- setup.py
+++ setup.py
@@ -1,36 +1,18 @@
-from setuptools import setup, find_packages
+#! /usr/bin/env python
+"""GDGUkraine package setuptools installer."""
+from setuptools import setup
 
-install_requires = [
-    'CherryPy==10.1.0',
-    'SQLAlchemy==1.0.11', 'yuicompressor==2.4.8', 'webassets==0.11.1',
-    'Routes==2.3.1', 'mysql-connector-python==2.0.4', 'WTForms==2.1',
-    'python-social-auth==0.2.13', 'alembic==0.8.4', 'openpyxl==2.3.2',
-]
 
-entry_points = {
-    'console_scripts': [
-        'load_gdg_fixtures = GDGUkraine.fixtures.loader:main',
+params = dict(
+    dependency_links=[
+        'https://cdn.mysql.com/Downloads/Connector-Python/'
+        'mysql-connector-python-2.0.4.zip'
+        '#md5=3df394d89300db95163f17c843ef49df'
     ],
-}
+    pbr=True,
+    package_dir={'': 'src'},
+    setup_requires=['pbr==3.0.0'],
+)
 
-setup(name='GDGUkraine',
-      version='1.0',
-      classifiers=[
-          'Environment :: Web Environment',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Topic :: Internet :: WWW/HTTP',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-      ],
-      author='Svyatoslav Sydorenko',
-      author_email='svyatoslav@sydorenko.org.ua',
-      package_dir={'': 'src'},
-      packages=find_packages('src', exclude=['test**']),
-      entry_points=entry_points,
-      install_requires=install_requires,
-      zip_safe=False)
+if __name__ == '__main__':
+    setup(**params)

35bce381beaadac9a3256c8738639a66949e8695
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-05-16 01:16:37 +0300

    Add deployment key to repo

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,7 @@
-#! /bin/sh
+#! /usr/bin/env bash
+
+BASEDIR=$(dirname "$0")
 
 echo This is a temporary placeholder for the deploymet scenario
+exit 0
+openssl aes-256-cbc -K "$encrypted_237af1bf8448_key" -iv "$encrypted_237af1bf8448_iv" -in "$BASEDIR/deploy_key.enc" -out "$BASEDIR/deploy_key" -d

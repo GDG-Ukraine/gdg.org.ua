bdfa98c323ca88f1e4ae6e19dc9d7bbbff8abf24
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-05-16 01:50:49 +0300

    Add decrypted key cleanup once deployment is over
    .. or when it's brutally interrupted ^_^

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,6 +3,15 @@
 BASEDIR=$(dirname "$0")
 DEPLOYMENT_KEY="$BASEDIR/deploy_key"
 
+function clean_up {
+    # Perform program exit housekeeping
+    echo Cleanning up stuff
+    rm -f "$DEPLOYMENT_KEY"
+    exit
+}
+
+trap clean_up SIGHUP SIGINT SIGTERM
+
 echo Decrypting a deployment key...
 openssl aes-256-cbc -K "$encrypted_237af1bf8448_key" -iv "$encrypted_237af1bf8448_iv" -in "${DEPLOYMENT_KEY}.enc" -out "$DEPLOYMENT_KEY" -d
 chmod 600 "$DEPLOYMENT_KEY"
@@ -14,3 +23,5 @@ echo Pushing the app to production...
 # Ref: http://stackoverflow.com/a/41612988/595220
 #GIT_SSH_COMMAND="ssh -i $DEPLOYMENT_KEY -F /dev/null" git push dokku master
 GIT_SSH_COMMAND="ssh -i $DEPLOYMENT_KEY" git push dokku master
+
+clean_up

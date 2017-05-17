0abecaa130493b334c7f4e4d16d0519f442f3356
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-05-17 14:16:52 +0300

    Move all requirements to requirements/* folder

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ params = dict(
     dependency_links=[
         'https://cdn.mysql.com/Downloads/Connector-Python/'
         'mysql-connector-python-2.0.4.zip'
-        '#md5=3df394d89300db95163f17c843ef49df'
+        '#md5=3df394d89300db95163f17c843ef49df',
+        '-e git+git://github.com/'
+        'open-craft-guild/blueberrypy@d41104c'
+        '#egg=blueberrypy[speedups]'
     ],
     pbr=True,
     package_dir={'': 'src'},

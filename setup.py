6ac4e32ecd55106a933d26a797719565042d1099
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-05-17 17:28:31 +0300

    Migrate from pbr to native setuptools+setuptools_scm

--- setup.py
+++ setup.py
@@ -4,17 +4,9 @@ from setuptools import setup
 
 
 params = dict(
-    dependency_links=[
-        'https://cdn.mysql.com/Downloads/Connector-Python/'
-        'mysql-connector-python-2.0.4.zip'
-        '#md5=3df394d89300db95163f17c843ef49df',
-        '-e git+git://github.com/'
-        'open-craft-guild/blueberrypy@d41104c'
-        '#egg=blueberrypy-0.6.0.dev[speedups]'
-    ],
-    pbr=True,
+    use_scm_version=True,
     package_dir={'': 'src'},
-    setup_requires=['pbr==3.0.0'],
+    setup_requires=['setuptools_scm>=1.15.0'],
 )
 
 if __name__ == '__main__':

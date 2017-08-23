b952789c849be45c237d92b1d85898ad1ce0ef67
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-08-24 00:37:31 +0300

    Adjust distribution setup options and meta

--- setup.py
+++ setup.py
@@ -2,12 +2,4 @@
 """GDGUkraine package setuptools installer."""
 from setuptools import setup
 
-
-params = dict(
-    use_scm_version=True,
-    package_dir={'': 'src'},
-    setup_requires=['setuptools_scm>=1.15.0'],
-)
-
-if __name__ == '__main__':
-    setup(**params)
+__name__ == '__main__' and setup()

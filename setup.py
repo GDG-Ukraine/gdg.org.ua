42a2bfcdc50a86c92b9ec702d1b5caa77d0c9142
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-10-24 14:25:28 +0300

    Improve distribution setup

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 #! /usr/bin/env python
 """GDGUkraine package setuptools installer."""
+
+__requires__ = ('setuptools>=36.6.0', )
+
+
 from setuptools import setup
 
-__name__ == '__main__' and setup()
+
+__name__ == '__main__' and setup(
+    use_scm_version=True,
+    setup_requires=[
+        'setuptools-git',
+        'setuptools-scm>=1.15.0',
+    ],
+)
